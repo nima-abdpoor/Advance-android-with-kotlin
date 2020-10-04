517a9ef8e57a3329a3f642c27af90a29e4de3a26
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-10-04 10:07:34 +0330

    first commit

---
+++ app/src/main/java/com/tinaio/customviews/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tinaio.customviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
